bb2627cd089f0c89dda62f4ec6ceb73691c9de9f
KarlisKocins <125027148+KarlisKocins@users.noreply.github.com>
2023-03-19 15:36:27 +0000

    First patch ALPHA ver 1.0

--- build_heap.py
+++ build_heap.py
@@ -2,37 +2,51 @@
 
 
 def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    def sift_down(i):
+        nonlocal swaps
+        min_index = i
+        left_child = 2 * i + 1
+        right_child = 2 * i + 2
 
+        if left_child < n and data[left_child] < data[min_index]:
+            min_index = left_child
+
+        if right_child < n and data[right_child] < data[min_index]:
+            min_index = right_child
+
+        if i != min_index:
+            swaps.append((i, min_index))
+            data[i], data[min_index] = data[min_index], data[i]
+            sift_down(min_index)
+
+    swaps = []
+    n = len(data)
+    for i in range(n // 2 - 1, -1, -1):
+        sift_down(i)
 
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
 
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    ievade = input("I or F: ")
+    if "I" in ievade:
+        n = int(input("Enter count: "))
+        data = list(map(int, input("Enter the numbers: ").split()))
 
     # checks if lenght of data is the same as the said lenght
+    elif "F" in ievade:
+
+        filename = input("File name: ")
+        with open("tests/"+filename, 'r') as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
+    print("Result:")
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
